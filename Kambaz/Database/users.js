d73d2bc80636dc9dc0b10f1ca101cc66110add36
Drumil23 <drumil.kotecha@gmail.com>
2025-11-21 14:42:50 -0500

    added additional user entries to the database

--- Kambaz/Database/users.js
+++ Kambaz/Database/users.js
@@ -1,4 +1,6 @@
 export default [
   { _id: "u1", username: "alice", password: "alice", role: "student" },
-  { _id: "u2", username: "bob", password: "bob", role: "instructor" }
+  { _id: "u2", username: "bob", password: "bob", role: "instructor" },
+  { _id: "u3", username: "drumil", password: "123", role: "dean" },
+  { _id: "u4", username: "test", password: "123", role: "student" }
 ];
