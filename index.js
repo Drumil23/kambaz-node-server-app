02ef6e876eb5beec1b874e99b0e9b543c80c2b4e
Drumil23 <drumil.kotecha@gmail.com>
2025-11-30 15:01:11 -0500

    refactored database structure, removed old data files and added new JSON data for assignments, courses, modules, and users; integrated mongoose for database connection

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import express from 'express';
+import mongoose from "mongoose";
+
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -9,6 +11,9 @@ import AssignmentRoutes from "./Kambaz/Assignments/routes.js";
 import session from "express-session"; 
 import "dotenv/config"; 
 const app = express()
+
+const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
+mongoose.connect(CONNECTION_STRING);
 
 app.use(cors({
     credentials: true,
