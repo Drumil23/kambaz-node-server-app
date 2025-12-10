5c0f9cf57f60573db0eb5b4f87f05ab3eb8e79df
Drumil23 <drumil.kotecha@gmail.com>
2025-12-09 19:29:11 -0500

    refactored enrollment and user DAO logic to use mongoose model; implement findAllEnrollments and update user-related functions

--- Kambaz/Enrollments/routes.js
+++ Kambaz/Enrollments/routes.js
@@ -1,6 +1,12 @@
 import * as dao from "./dao.js";
+import model from "./model.js";
 
 export default function EnrollmentsRoutes(app) {
+  const findAllEnrollments = async (req, res) => {
+    const enrollments = await model.find();
+    res.json(enrollments);
+  };
+
   const enrollUserInCourse = async (req, res) => {
     const { userId, courseId } = req.params;
     const enrollment = await dao.enrollUserInCourse(userId, courseId);
@@ -13,6 +19,7 @@ export default function EnrollmentsRoutes(app) {
     res.json(status);
   };
 
+  app.get("/api/enrollments", findAllEnrollments);
   app.post("/api/enrollments/:userId/:courseId", enrollUserInCourse);
   app.delete("/api/enrollments/:userId/:courseId", unenrollUserFromCourse);
 }
